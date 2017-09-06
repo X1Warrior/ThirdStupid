76c0276de9cd5c44a2f6ab5ed2a1b655e4c8c4f9
X1Warrior <zsto7638@gmail.com>
2017-09-06 13:16:19 -0600

    Initial commit

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,13 @@
+package stupid.controller;
+/**
+ * 
+ * @author Zach Stout
+ *
+ */
+public class StupidController
+{
+	public void start()
+	{
+		System.out.println("I love to play soccer and eat food");
+	}
+}
