2689fe08418441c00b1efc9e5db3ba31f4e52ada
X1Warrior <zsto7638@gmail.com>
2017-09-07 13:52:40 -0600

    learned about scanner

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,4 +1,6 @@
 package stupid.controller;
+
+import java.util.scanner;
 /**
  * 
  * @author Zach Stout
@@ -7,7 +9,19 @@ package stupid.controller;
 public class StupidController
 {
 	public void start()
-	{
+	{	
 		System.out.println("I love to play soccer and eat food");
 	}
+}
+	testScanner();
+{
+	
+}
+	private void testScanner()
+{
+	   Scanner firstScanner;
+	   firstScanner = new Scanner(System.in);
+	   system.out.prinln("what is your favorite food?")
+	   String answer = firstScannerr.next();
+	   System.out.println("Oh cool, you like" + answer);
 }
